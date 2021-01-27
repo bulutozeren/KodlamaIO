296c7edcc5593a6aa1910c310663f2808c11e681
bulutozeren <bulutozeren@hotmail.com>
2021-01-27 18:31:44 +0300

    4. gün ödevi, MyDictionary

---
+++ Odev.Dictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev.Dictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private DictionaryObject<TKey, TValue>[] dictionaryObjects;
+
+        public MyDictionary()
+        {
+            dictionaryObjects = new DictionaryObject<TKey, TValue>[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var tempDictionaryObjects = dictionaryObjects;
+            dictionaryObjects = new DictionaryObject<TKey, TValue>[dictionaryObjects.Length + 1];
+
+            for (int i = 0; i < tempDictionaryObjects.Length; i++)
+            {
+                dictionaryObjects[i] = tempDictionaryObjects[i];
+            }
+
+            dictionaryObjects[dictionaryObjects.Length - 1] = new DictionaryObject<TKey, TValue> { Key = key, Value = value };
+        }
+
+        public int Length
+        {
+            get { return dictionaryObjects.Length; }
+        }
+
+        public TValue[] Values
+        {
+            get
+            {
+                TValue[] values = new TValue[dictionaryObjects.Length];
+                for (int i = 0; i < dictionaryObjects.Length; i++)
+                {
+                    values[i] = dictionaryObjects[i].Value;
+                }
+                return values;
+            }
+        }
+
+        public class DictionaryObject<TKey, TValue>
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+        }
+    }
+}
---
+++ Odev.Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Odev.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Engin");
+            Console.WriteLine(myDictionary.Length);
+
+            myDictionary.Add(2, "Berkay");
+            Console.WriteLine(myDictionary.Length);
+
+            foreach (var value in myDictionary.Values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
