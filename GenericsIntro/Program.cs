27df1f61496bde2fd73ffb35d21764a189f65608
bulutozeren <bulutozeren@hotmail.com>
2021-01-27 17:55:24 +0300

    4. gün ders kodları

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Engin");
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Kerem");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product masa = new Product
+            {
+                Id = 1,
+                CategoryId = 2,
+                Name = "Masa",
+                UnitPrice = 500,
+                UnitsInStock = 3
+            };
+
+            Product product = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                Name = "Kalem",
+                UnitPrice = 35,
+                UnitsInStock = 5
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
